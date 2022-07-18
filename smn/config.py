e907ece4d4ef10c8ee89ef34a3563ad2ce465f5c
Utya based <53794217+unxho@users.noreply.github.com>
2022-07-18 22:29:30 +0300

    fix config

--- smn/config.py
+++ smn/config.py
@@ -9,11 +9,11 @@ __config.read('config.ini')
 
 API_ID, API_HASH = __config.get("api", "id"), __config.get("api", "hash")
 BOT_TOKEN = __config.get("bot", "token")
-HELPER_ENABLED, HELPER_PHONE = __config.get("helper", "use"), \
+HELPER_ENABLED, HELPER_PHONE = __config.getboolean("helper", "use"), \
                                __config.get("helper", "phone")
 CHANNEL, LOG_CHAT = int(__config.get("target", "channel")), \
                     int(__config.get("target", "log_chat"))
-FALLBACK, FALLBACK_TIMEOUT = __config.get("posting", "fallback"), \
+FALLBACK, FALLBACK_TIMEOUT = __config.getboolean("posting", "fallback"), \
                              int(__config.get("posting", "fallback_timeout"))
 
 if not HELPER_ENABLED and FALLBACK:
