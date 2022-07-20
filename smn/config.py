5a63b0fffd9bcc9db9c73e1aca63066896d23b92
W <53794217+unxho@users.noreply.github.com>
2022-07-20 18:33:58 +0300

    update config

--- smn/config.py
+++ smn/config.py
@@ -1,21 +1,27 @@
 from configparser import ConfigParser
 import os
+import sys
 
 __config = ConfigParser()
 if 'config.ini' not in os.listdir():
     print("No config file found")
-    exit(1)
+    sys.exit(1)
 __config.read('config.ini')
 
-API_ID, API_HASH = __config.get("api", "id"), __config.get("api", "hash")
+API_ID = __config.getint("api", "id")
+API_HASH = __config.get("api", "hash")
+
 BOT_TOKEN = __config.get("bot", "token")
-HELPER_ENABLED, HELPER_PHONE = __config.getboolean("helper", "use"), \
-                               __config.get("helper", "phone")
-CHANNEL, LOG_CHAT = int(__config.get("target", "channel")), \
-                    int(__config.get("target", "log_chat"))
-FALLBACK, FALLBACK_TIMEOUT = __config.getboolean("posting", "fallback"), \
-                             int(__config.get("posting", "fallback_timeout"))
+
+HELPER_ENABLED = __config.getboolean("helper", "use")
+HELPER_PHONE = __config.get("helper", "phone")
+
+CHANNEL = __config.getint("target", "channel")
+LOG_CHAT = __config.getint("target", "log_chat")
+
+FALLBACK = __config.getboolean("posting", "fallback")
+FALLBACK_TIMEOUT = __config.getint("posting", "fallback_timeout")
 
 if not HELPER_ENABLED and FALLBACK:
     print('CONFIG CONFLICT: HELPER MUST BE PRESENT TO USE FALLBACK MODE')
-    exit(1)
+    sys.exit(1)
